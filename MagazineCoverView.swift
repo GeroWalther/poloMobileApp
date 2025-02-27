29742282e47dfc91d7fb4bb6fcfe2b549fddc0cb
Amit <amitsanvedi5@gmail.com>
2025-02-27 16:13:34 +0530

    Code refector

--- MagazineCoverView.swift
+++
@@ -1,6 +0,0 @@
-struct MagazineCoverView: View {
-    let magazine: Magazine
-    @State private var isLoadingThumbnail = true
-    @State private var loadError: Error?
-    @EnvironmentObject private var viewModel: MagazineViewModel
-} 
